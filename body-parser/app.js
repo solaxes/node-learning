863997966eef835a2288c042dffa5f7bf07c1b32
Jaidev Bangar <solaxes@gmail.com>
2021-04-09 07:39:37 +0530

    body-parser final commit

--- body-parser/app.js
+++ body-parser/app.js
@@ -1,23 +1,32 @@
 //load depencies
 var express = require('express');
+const bodyParser = require('body-parser');
+
 
 // create application object
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 
 //create routing
 
-app.use('/product',(req, res) => {
-    console.log(req.body);
-
-    res.redirect('/');
+app.use('/add-product',(req, res) => {
+    
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Save</button></form>');
+    
 } );
+
+app.post('/product', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
 
 app.use('/', (req, res)=> {
     res.send('<h1>Hello from express</h1>');
 });
 
 app.listen(3000, ()=>{
-    console.log("server isrunning");
+    console.log("server is running");
 });
 
